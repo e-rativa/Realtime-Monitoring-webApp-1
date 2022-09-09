b59f3df7c2cd110762989c4addaaf6f57169dc60
oscar cortes <oe.cortes@uniandes.edu.co>
2022-09-09 16:59:05 -0500

    test

--- realtimeMonitoring/realtimeGraph/urls.py
+++ realtimeMonitoring/realtimeGraph/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('historical/data',
          download_csv_data, name='historical-data'),
+    path("semana5/<str:measure>", get_semana_5, name="semana5"),
 ]
