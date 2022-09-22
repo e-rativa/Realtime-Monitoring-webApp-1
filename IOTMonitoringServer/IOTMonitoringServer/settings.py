dee116136944d59261dc7d1d064ba343f59874a4
Elkin Rativa <elkinming@gmail.com>
2022-09-22 14:31:19 -0500

    Nuevas IP de Servidores

--- IOTMonitoringServer/IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "54.89.195.187"]
+ALLOWED_HOSTS = ["localhost", "54.234.248.150"]
 
 
 # Application definition
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "54.174.34.251",  # Dirección IP de la base de datos
+        "HOST": "52.91.206.159",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "54.163.73.52"
+MQTT_HOST = "35.170.185.246"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
