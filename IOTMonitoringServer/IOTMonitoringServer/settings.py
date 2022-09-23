460c45b6e19578ed13fb9f54606c53b26af75147
Elkin Rativa <elkinming@gmail.com>
2022-09-23 16:06:03 -0500

    feat: Configurando IPs

--- IOTMonitoringServer/IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/IOTMonitoringServer/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "18.212.39.106",  # Dirección IP de la base de datos
+        "HOST": "44.204.229.1",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
